6ef5ffc6b695635e420d4f658d22aa526daa57b1
AmyKelly56 <amyannakelly@outlook.com>
2017-02-28 12:26:04 +0000

    Update UI Function

--- App/PlayListViewController.swift
+++ App/PlayListViewController.swift
@@ -23,16 +23,36 @@ class PlayListViewController: UIViewController {
     @IBOutlet weak var cover11: UIImageView!
     @IBOutlet weak var cover12: UIImageView!
     
+    var covers: [UIImageView]!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+         covers = [cover1, cover2, cover3, cover4, cover5, cover6, cover7, cover8, cover9, cover10, cover11, cover12];
+        
+        updateUI()
+        
     }
+    
+    func updateUI()
+    {
+        for coverImage in covers {
+            //Testing: change cover image
+            coverImage.image = UIImage(named: "Ghost Stories")
+        }
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
+        
+    
     }
 
 
