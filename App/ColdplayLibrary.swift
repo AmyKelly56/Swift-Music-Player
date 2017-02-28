e07f69e511ff41a5df35d5411a733b0268d47afb
AmyKelly56 <amyannakelly@outlook.com>
2017-02-28 13:22:19 +0000

    Data Added To Model

--- App/ColdplayLibrary.swift
+++ App/ColdplayLibrary.swift
@@ -10,5 +10,71 @@ import Foundation
 
 struct ColdplayLibrary {
     
-    let album = 
+    let album : [String : AnyObject] = [
+        "title" : "Ghost Stories",
+        "description" : "Ghost Stories is the sixth studio album by the British rock band Coldplay. Co-produced by the band with Paul Epworth along with returning Mylo Xyloto producers Daniel Green and Rik Simpson, it was released by Parlophone on 16 May 2014.",
+        "coverImageName" : "Ghost Stories",
+        "songs" : ["Always In My Head", "Magic", "Ink", "True Love", "Midnight", "Anothers Arms", "Sky Full of Stars", "O", "Oceans"]
+        
+    ]
+    
+    
+    let albums : [[String : AnyObject]] = [
+        [
+            "title" : "Ghost Stories",
+            "description" : "Ghost Stories is the sixth studio album by the British rock band Coldplay. Co-produced by the band with Paul Epworth along with returning Mylo Xyloto producers Daniel Green and Rik Simpson, it was released by Parlophone on 16 May 2014.",
+            "coverImageName" : "Ghost Stories",
+            "songs" : ["Always In My Head", "Magic", "Ink", "True Love", "Midnight", "Anothers Arms", "Sky Full of Stars", "O", "Oceans"]
+        ],
+        [
+            "title" : "A Head Full of Dreams",
+            "description" : "A Head Full of Dreams is the seventh studio album by the British rock band Coldplay. It was released on 4 December 2015 by Parlophone and Atlantic, with a 5.1 Surround Blu-ray Audio version being released via the band's website on 23 September 2016.",
+            "coverImageName" : "A Head Full of Dreams",
+            "song" : ["A Head Full of Dreams", "Birds", "Everglow", "Hymn for the Weekend", "Adventure of a Lifetime", "Fun', Army of One", "Kaleidoscope", "Amazing Day", "Color Spectrum", "Up&Up"]
+        ],
+        [
+            "title" : "Mylo Xyloto",
+            "description" : "Mylo Xyloto /ˈmaɪloʊ ˈzaɪlətoʊ/ is fifth studio album by the British alternative rock band Coldplay. It was first released on 19 October 2011 by EMI, and later worldwide on 24 October.",
+            "coverImageName" : "Mylo Xyloto",
+            "songs" : ["Mylo Xyloto", "Hurts Like Heaven", "Paradise", "Charlie Brown", "Us Against The World", "M.M.I.X", "Every Teardrop is a Waterfall", "Major Minus", "UFO", "Princess of China", "Up in Flames", "A Hopeful Transmission", "Don't Let it Break Your Heart", "Up with the Birds"]
+        ],
+        [
+            "title" : "Viva la Vida or Death and All His Friends",
+            "description" : "Viva la Vida or Death and All His Friends, often referred to as simply Viva la Vida, is the fourth studio album by the British alternative rock band Coldplay, released on 12 June 2008 on Parlophone.",
+            "coverImageName" : "Viva la Vida or Death and All His Friends",
+            "songs" : ["Viva La Vida", "Life in Technicolor", "Cemiteries in London", "Lost!", "42", "Lovers in Japan", "Yes", "Violet Hill", "Strawberry Swing", "Death and All His Friends"]
+        ],
+        [
+            "title" : "X&Y",
+            "description" : "X&Y is the third studio album by the British alternative rock band Coldplay, released on 6 June 2005 worldwide by the record label Parlophone. The album was produced by the band, and British record producer Danton Supple.",
+            "coverImageName" : "X&Y",
+            "songs" : ["Square One", "What If", "White Shadows", "Fix You", "Talk", "X & Y", "Speed of Sounds", "A Message", "Low", "The Hardest Part", "Swalloed in the Sea", "Twisted Logic", "Til Kindom Come"]
+        ],
+        [
+            "title" : "A Rush of Blood to the Head",
+            "description" : "A Rush of Blood to the Head is the second studio album by the British alternative rock band Coldplay. Released on 26 August 2002 in the UK through the label Parlophone, the album was produced by the band and British record producer Ken Nelson.",
+            "coverImageName" : "A Rush of Blood to the Head",
+            "songs" : ["Politik", "In My Place", "God Put A Smile Upon Your Face", "The Scientist", "Clocks", "Daylight", "Green Eyes", "Warning Sign", "A Whisper", "A Rush of Blood to the Head", "Amsterdam"]
+        ],
+        [
+            "title" : "Parachutes",
+            "description" : "Parachutes is the debut studio album by the British rock band Coldplay. It was released on 10 July 2000 by Parlophone in the United Kingdom.",
+            "coverImageName" : "Parachutes",
+            "songs" : ["Don't Panic", "Shiver", "Spies", "Sparks", "Yellow", "Trouble", "Parachutes", "High Speed", "We Never Change", "Everyhtings Not Lost"]
+            
+        ],
+        [
+            "title" : "Safety",
+            "description" : "Safety was the first public release by the British rock band Coldplay. It was recorded over a weekend during May 1998, and was intended as a demo for record companies. It was financed by the band and their manager Phil Harvey for around £1500.",
+            "coverImageName" : "Safety",
+            "songs" : ["Bigger Stronger", "No More Keeping My Feet On The Ground", "Such a Rush"]
+        ]
+    
+    
+    
+    
+    ]
+    
+    
+    
 }
