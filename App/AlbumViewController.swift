10f17eb8f13a093559ccddfc3554f244fba60494
AmyKelly56 <amyannakelly@outlook.com>
2017-03-01 15:08:41 +0000

    Album View Class

--- App/AlbumViewController.swift
+++ App/AlbumViewController.swift
@@ -10,26 +10,8 @@ import UIKit
 
 class AlbumViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var albumCoverImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
