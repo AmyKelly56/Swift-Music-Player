2868efb03c64c97adbd65dd83d0065e6e3357f93
AmyKelly56 <amyannakelly@outlook.com>
2017-03-09 17:00:49 +0000

    Deletions

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -8,19 +8,12 @@
 
 import UIKit
 
-let appDelegate = UIApplication.shared.delegate as! AppDelegate
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, SPTAudioStreamingDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var session: SPTSession?
-    var player: SPTAudioStreamingController?
-    let kClientId = "2932d20ab2e5426dba6becb2e8c61bcd"
-    let kCallbackURL = "SwiftPlayer://returnAfterLogin"
-    let kTokenSwapURL = "http://localhost:1234/swap"
-    let kTokenRefreshServiceURL = "http://localhost:1234/refresh"
-    let kSessionUserDefaultsKey = "SpotifySession"
+
 
     func delay(_ delay:Double, closure:@escaping ()->()) {
         DispatchQueue.main.asyncAfter(
@@ -29,37 +22,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, SPTAudioStreamingDelegate
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        SPTAuth.defaultInstance().clientID = kClientId
-        SPTAuth.defaultInstance().redirectURL = URL(string:kCallbackURL)
-        //SPTAuth.defaultInstance().tokenSwapURL = URL(string:kTokenSwapURL)
-        SPTAuth.defaultInstance().requestedScopes = [SPTAuthStreamingScope]
-        //SPTAuth.defaultInstance().tokenRefreshURL = URL(string: kTokenRefreshServiceURL)!
-        SPTAuth.defaultInstance().sessionUserDefaultsKey = kSessionUserDefaultsKey
 
         
         return true
     }
     
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-        // Ask SPTAuth if the URL given is a Spotify authentication callback
-        
-        print("The URL: \(url)")
-        if SPTAuth.defaultInstance().canHandle(url) {
-            SPTAuth.defaultInstance().handleAuthCallback(withTriggeredAuthURL: url) { error, session in
-                // This is the callback that'll be triggered when auth is completed (or fails).
-                if error != nil {
-                    print("*** Auth error: \(error)")
-                    return
-                }
-                else {
-                    SPTAuth.defaultInstance().session = session
-                }
-                NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "sessionUpdated"), object: self)
-            }
-        }
-        return false
-    }
 
 
     func applicationWillResignActive(_ application: UIApplication) {
