ae3f96a523a9ce9c5f6e124023c04a3a636b9f8a
AmyKelly56 <amyannakelly@outlook.com>
2017-04-01 13:24:37 +0100

    TableViewCell

--- App/SongTableViewCell.swift
+++ App/SongTableViewCell.swift
@@ -10,9 +10,10 @@ import UIKit
 
 class SongTableViewCell: UITableViewCell {
     
-    @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var coverImageView: UIImageView!
+    @IBOutlet weak var songTitleLabel: UILabel!
     @IBOutlet weak var artistLabel: UILabel!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
