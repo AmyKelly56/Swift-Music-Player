88084d04283a4b02e20661cd46a342c94c8ad9de
AmyKelly56 <amyannakelly@outlook.com>
2017-04-04 13:15:01 +0100

    Added MP3 Files

--- App/MusicLibrary.swift
+++ App/MusicLibrary.swift
@@ -10,6 +10,28 @@ import Foundation
 
 struct MusicLibrary {
 
-    var library = [["title":"Adventure of a Lifetime","artist":"Coldplay","index":"0","coverImage":"0"],["title":"Stay","artist":"Zedd & Alessia Cara","index":"1","coverImage":"1"],["title":"You Don't Know Me","artist":"Jax Jones","index":"2","coverImage":"2"],["title":"Symphony","artist":"Clean Bandit ft. Zara Larsson","index":"3","coverImage":"3"],["title":"Galway Girl","artist":"Ed Sheeran","index":"4","coverImage":"4"],["title":"Rozes","artist":"The Chainsmokers","index":"5","coverImage":"5"],["title":"Something Just Like This","artist":"The Chainsmokers & Coldplay","index":"6","coverImage":"6"],["title":"Starboy","artist":"The Weekend","index":"7","coverImage":"7"],["title":"How Deep Is Your Love","artist":"Clavin Harris","index":"8","coverImage":"8"],["title":"Drunk In Love","artist":"Beyonce","index":"9","coverImage":"9"]]
+    var library = [
+        
+                    ["title":"Adventure of a Lifetime","artist":"Coldplay","index":"0","coverImage":"0"],
+                    ["title":"Stay","artist":"Zedd & Alessia Cara","index":"1","coverImage":"1"],
+                    ["title":"You Don't Know Me","artist":"Jax Jones","index":"2","coverImage":"2"],
+                    ["title":"Symphony","artist":"Clean Bandit ft. Zara Larsson","index":"3","coverImage":"3"],
+                    ["title":"Galway Girl","artist":"Ed Sheeran","index":"4","coverImage":"4"],
+                    ["title":"Rozes","artist":"The Chainsmokers","index":"5","coverImage":"5"],
+                    ["title":"Something Just Like This","artist":"The Chainsmokers & Coldplay","index":"6","coverImage":"6"],
+                    ["title":"Starboy","artist":"The Weekend","index":"7","coverImage":"7"],
+                    ["title":"How Deep Is Your Love","artist":"Clavin Harris","index":"8","coverImage":"8"],
+                    ["title":"Drunk In Love","artist":"Beyonce","index":"9","coverImage":"9"],
+                    ["title":"Hello","artist":"Adele","index":"10","coverImage":"10"],
+                    ["title":"Pompeii","artist":"Bastille","index":"11","coverImage":"11"],
+                    ["title":"Summer","artist":"Calvin Harris","index":"12","coverImage":"12"],
+                    ["title":"Burn","artist":"Ellie Goulding","index":"13","coverImage":"13"],
+                    ["title":"Sorry","artist":"Justin Bieber","index":"14","coverImage":"14"],
+                    ["title":"Uptown Funk","artist":"Mark Ronson ft. Bruno Mars","index":"15","coverImage":"15"],
+                    ["title":"Blurred Lines","artist":"Robin Thicke","index":"16","coverImage":"16"],
+                    ["title":"Chasing Cars","artist":"Snow Patrol","index":"17","coverImage":"17"],
+                    ["title":"Dont You Worry","artist":"Swedish House Mafia","index":"18","coverImage":"18"],
+                    ["title":"Moves Like Jagger","artist":"Maroon 5","index":"19","coverImage":"19"],
+    ]
     
 }
